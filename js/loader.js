d6d6e8bd142e5424543d386d892796872c1d9ca7
prog-harsh <ht87374@gmail.com>
2021-10-06 23:09:48 +0530

    added loader and updated style of footer

---
+++ js/loader.js
@@ -0,0 +1,12 @@
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.querySelector("header").style.display = "block";
+    document.querySelector("footer").style.display = "block";
+    document.querySelector(".main-loader").style.height = 0;
+}
