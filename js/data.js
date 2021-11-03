c4de664e410424b67dc7ac028a385f8bf49e469c
prog-harsh <ht87374@gmail.com>
2021-11-03 18:09:21 +0530

    added about-us and contact-us page

--- js/data.js
+++ js/data.js
@@ -9,18 +9,18 @@ const data = [{
         price: '89'
     },
     {
-        image: 'https://foodro.in/media/category//category-16160702352006.jpeg',
-        title: 'Daily Products',
+        image: 'https://www.bigbasket.com/media/uploads/p/l/274145_14-fortune-sun-lite-sunflower-refined-oil.jpg',
+        title: 'Sun Lite - Sunflower Refined Oil',
+        price: '150'
+    },
+    {
+        image: 'https://www.bigbasket.com/media/uploads/p/l/40007334_12-oleev-active-goodness-of-olive-oil.jpg',
+        title: 'Oleev Active - Goodness Of Olive Oil',
         price: '150'
     },
     {
         image: 'https://foodro.in/media/category//category-16160702352006.jpeg',
         title: 'Daily Products',
-        price: '150'
-    },
-    {
-        image: 'https://foodro.in/media/category//category-16160702352006.jpeg',
-        title: 'Daily Products',
-        price: '150'
+        price: '251.75'
     }
 ]
