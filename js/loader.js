097b9bc4cf5783d0401aa982ba41c813d682916c
prog-harsh <ht87374@gmail.com>
2021-11-04 13:46:19 +0530

    added ready to cook page and some minor update

--- js/loader.js
+++ js/loader.js
@@ -7,6 +7,7 @@ function myFunction() {
 function showPage() {
     document.getElementById("loader").style.display = "none";
     document.querySelector("header").style.display = "block";
+    document.querySelector(".header-img").style.display = "block";
     document.querySelector("footer").style.display = "block";
     document.querySelector("main").style.display = "block";
     document.querySelector(".main-loader").style.height = 0;
---
+++ js/readyToCookData.js
@@ -0,0 +1,21 @@
+const data = [{
+        image: 'https://cdn.shopify.com/s/files/1/0173/7644/4470/products/ChickenChettinad-01_44fc519d-e42e-4022-adc7-792d2b1bf417_1024x1024.jpg?v=1605611013',
+        title: 'Ready to Eat Chicken Chettinad',
+        price: '180'
+    },
+    {
+        image: 'https://rukminim1.flixcart.com/image/416/416/khwbde80-0/popcorn/v/p/i/hot-fresh-microwave-microwave-act-ii-original-imafxstmnffbve6z.jpeg?q=70',
+        title: 'ACT II Hot & Fresh Microwave Butter Lovers Popcorn',
+        price: '99'
+    },
+    {
+        image: 'https://www.bigbasket.com/media/uploads/p/l/1214117_1-id-fresho-idly-dosa-batter-1-kg-whole-wheat-parota-350-g.jpg',
+        title: 'Idly Dosa Batter, 1 Kg + Whole Wheat Parota',
+        price: '149'
+    },
+    {
+        image: 'https://cdn.shopify.com/s/files/1/0173/7644/4470/products/FOP_4ebe8798-b6ba-4ff0-8915-f87a2deeef04_1024x1024.jpg?v=1605959434',
+        title: 'Nawabi Kaju Halwa and Awadhi Badam Halwa',
+        price: '599'
+    }
+]
